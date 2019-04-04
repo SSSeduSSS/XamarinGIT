d04b011b9b68e97b5ace52c8bb0d5ad9bed29b07
Eduardo Bermúdez Blanco <Eduardo.Bermudez@tajamar365.com>
2019-04-04 14:07:39 +0200

    final clase

---
+++ XamarinSolution/TiposControles/TiposControles/OtrosControles.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TiposControles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OtrosControles : ContentPage
+	{
+        bool ventanaActiva;
+		public OtrosControles ()
+		{
+			InitializeComponent ();
+            this.progreso.Progress = 0;
+
+            this.slider1.ValueChanged += Slider1_ValueChanged;
+            this.stepper1.ValueChanged += Stepper1_ValueChanged;
+		}
+
+        private void Stepper1_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            this.lblStepper.Text = "Stepper : " + e.NewValue;
+        }
+
+        private void Slider1_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            this.lblSlider.Text = "Slider: " +e.NewValue;
+        }
+
+        protected override void OnAppearing()
+        {
+            //Cuando la ventana aparece se ejecuta este método
+            base.OnAppearing();
+            this.ventanaActiva = true;
+            //Device se comunica con el sistema operativo del dispositivo
+            //StartTimer es compatible con todos los OS
+            
+            //Cada 0.1 Segundos hace una llamada a TimerCallBack
+            Device.StartTimer(TimeSpan.FromSeconds(0.1),TimerCallBack);
+        }
+
+        bool TimerCallBack ()
+        {
+            this.progreso.Progress += 0.01;
+
+            //Si llega a 1 o si se cierra la ventana realiza un return
+            return ventanaActiva || this.progreso.Progress == 1;
+        }
+
+        protected override void OnDisappearing()
+        {
+            //Cuando la ventana desaparece se ejecuta este método
+            base.OnDisappearing();
+            this.ventanaActiva = false;
+        }
+    }
+}
---
+++ XamarinSolution/TiposControles/TiposControles/PickerView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TiposControles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerView : ContentPage
+    {
+        List<String> ingredientes;
+        public PickerView()
+        {
+            InitializeComponent();
+            ingredientes = new List<String>() { };
+            foreach (String s in ingredientes)
+            {
+                this.picker1.Items.Add(s);
+            }
+            //Es otra forma de crear el evento, con función anónima
+            this.picker1.SelectedIndexChanged += (sender, args) =>
+             {
+                 int indice = this.picker1.SelectedIndex;
+                 if(indice==-1)
+                 {
+                     this.lblPicker.Text = "No ha seleccionado nada";
+                 }
+                 else
+                 {
+                     this.lblPicker.Text = "Ha seleccionado: "+ this.ingredientes[indice];
+                 }
+             };
+        }
+
+    }
+
+}
